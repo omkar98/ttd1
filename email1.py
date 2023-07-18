d37c9dcd0cb2ca9df36b3abbcac0ea4fcef55127
Omkar Deshpande <edu.omkar@gmail.com>
2023-07-18 22:05:00 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -14,30 +14,30 @@ if __name__ == "__main__":
     url = "https://online.tirupatibalaji.ap.gov.in/sdn/rest/v1/acc/get_availability?for=dashboard&location=TIRUMALA"
     payload={}
     headers = {
-    'authority': 'online.tirupatibalaji.ap.gov.in',
-    'accept': 'application/json',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
-    'authtoken': 'edbe994e5459905930a8884e16e49892d16288f31639199834',
-    'content-type': 'application/json',
-    'cookie': '_ga=GA1.5.834326753.1670903926; _gid=GA1.5.2036348688.1684908791; _gid=GA1.3.1408227557.1685176566; _ga=GA1.1.834326753.1670903926; user_id=9441154; authtoken=edbe994e5459905930a8884e16e49892d16288f31639199834; valid_till=2026-05-27 14:37:08; valid_till_in_utc=2026-05-27 09:07:08; mobileNum=8669175196; _gat_UA-56816637-81=1; _ga_MT8JLQ6M96=GS1.1.1685176570.16.1.1685176693.0.0.0',
-    'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
-    'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-dest': 'empty',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-site': 'same-origin',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
-    'userid': '9441154'
+        'authority': 'online.tirupatibalaji.ap.gov.in',
+        'accept': 'application/json',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,hi;q=0.7',
+        'authtoken': 'edbe994e5459905930a8884e16e49892d16288f31639199834',
+        'content-type': 'application/json',
+        'cookie': '_ga=GA1.5.834326753.1670903926; _gid=GA1.5.2036348688.1684908791; _gid=GA1.3.1408227557.1685176566; _ga=GA1.1.834326753.1670903926; user_id=9441154; authtoken=edbe994e5459905930a8884e16e49892d16288f31639199834; valid_till=2026-05-27 14:37:08; valid_till_in_utc=2026-05-27 09:07:08; mobileNum=8669175196; _gat_UA-56816637-81=1; _ga_MT8JLQ6M96=GS1.1.1685176570.16.1.1685176693.0.0.0',
+        'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
+        'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36',
+        'userid': '9441154'
     }
     logger.warning("url: "+str(url)+" | payload: "+str(payload)+ "| headers: " +str(headers)) 
     try:
-    response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload)
     except:
-    logger.critical("ERROR from TTD")
-    break
+        logger.critical("ERROR from TTD")
+        break
     if(not response.json().get('status') == 'fail'):
-    avl = (response.json().get('result').get('20230723').get('avl'))
+        avl = (response.json().get('result').get('20230723').get('avl'))
     if (avl==0): 
       avl = (response.json().get('result').get('20230724').get('avl'))
     message = 'Subject: {}\n\n{}'.format(json.dumps(response.json().get('result').get('20230723')), "Available for 23/24 July")
