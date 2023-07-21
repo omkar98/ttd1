fabd736389da10be81add11c1d34f95f1c401188
Omkar Deshpande <edu.omkar@gmail.com>
2023-07-21 10:48:33 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -1,25 +1,36 @@
+import smtplib
+import logging 
 import requests
 import json
+import time
 
-url = "https://online.tirupatibalaji.ap.gov.in/cms/api/notifications"
+counter = 1
 
-payload = {}
-headers = {
-  'authority': 'online.tirupatibalaji.ap.gov.in',
-  'accept': 'application/json',
-  'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-  'content-type': 'application/json',
-  'cookie': '_ga=GA1.5.1068875658.1682002300; _gid=GA1.5.1199907862.1689691573; _gat_UA-56816637-81=1; _gid=GA1.3.508613201.1689916322; _gat=1; _ga=GA1.1.1068875658.1682002300; _ga_MT8JLQ6M96=GS1.1.1689916320.26.1.1689916347.0.0.0',
-  'referer': 'https://online.tirupatibalaji.ap.gov.in/home/dashboard',
-  'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-  'sec-ch-ua-mobile': '?1',
-  'sec-ch-ua-platform': '"Android"',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-origin',
-  'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36'
-}
+logging.basicConfig()
 
-response = requests.request("GET", url, headers=headers, data=payload)
-
-print(response.text)
+if __name__ == "__main__":
+    logger = logging.getLogger("Syslog")
+    logger.warning("Inside Main")
+    while True:
+      url = "https://online.tirupatibalaji.ap.gov.in/sdn/rest/v1/acc/get_availability?for=dashboard&location=TIRUMALA"
+      payload={}
+      headers = {
+          'authority': 'online.tirupatibalaji.ap.gov.in',
+          'accept': 'application/json',
+          'authtoken': '16202e94375972563f5f563e2647eb58b690c22d-1245772922',
+          'content-type': 'application/json',
+          'cookie': '_ga=GA1.5.1068875658.1682002300; _gid=GA1.5.1199907862.1689691573; _ga=GA1.1.1068875658.1682002300; _gat_UA-56816637-81=1; user_id=9441154; authtoken=16202e94375972563f5f563e2647eb58b690c22d-1245772922; valid_till=2024-07-21 10:24:32; valid_till_in_utc=2024-07-21 04:54:32; mobileNum=8669175196; _ga_MT8JLQ6M96=GS1.1.1689913461.25.1.1689913479.0.0.0',
+          'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
+          'userid': '9441154'
+        }
+      proceed = True
+      try:
+          for x in range(6):
+            response = requests.request("GET", url, headers=headers, data=payload)
+            logger.warning("response: "+str(response) + "Status Code is: "+str(response.status_code))
+            print(response.text)
+            if response.status_code != 200:
+                proceed = False
+      except:
+        proceed = False
+        logger.critical("ERROR from TTD")
