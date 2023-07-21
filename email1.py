a283a87282a5d2818b7f25c2e8913ed1d9783849
Omkar Deshpande <edu.omkar@gmail.com>
2023-07-21 10:29:08 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -17,16 +17,10 @@ if __name__ == "__main__":
       headers = {
           'authority': 'online.tirupatibalaji.ap.gov.in',
           'accept': 'application/json',
-          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
           'authtoken': '16202e94375972563f5f563e2647eb58b690c22d-1245772922',
           'content-type': 'application/json',
           'cookie': '_ga=GA1.5.1068875658.1682002300; _gid=GA1.5.1199907862.1689691573; _ga=GA1.1.1068875658.1682002300; _gat_UA-56816637-81=1; user_id=9441154; authtoken=16202e94375972563f5f563e2647eb58b690c22d-1245772922; valid_till=2024-07-21 10:24:32; valid_till_in_utc=2024-07-21 04:54:32; mobileNum=8669175196; _ga_MT8JLQ6M96=GS1.1.1689913461.25.1.1689913479.0.0.0',
           'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
-          'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-          'sec-ch-ua-mobile': '?1',
-          'sec-fetch-dest': 'empty',
-          'sec-fetch-mode': 'cors',
-          'sec-fetch-site': 'same-origin',
           'userid': '9441154'
         }
       proceed = True
