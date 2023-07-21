6ca8fe7b2312a2831615ea157323042e352a24b4
Omkar Deshpande <edu.omkar@gmail.com>
2023-07-21 10:14:07 +0530

    Update email1.py

--- email1.py
+++ email1.py
@@ -11,32 +11,33 @@ logging.basicConfig()
 if __name__ == "__main__":
     logger = logging.getLogger("Syslog")
     logger.warning("Inside Main")
-    url = "https://online.tirupatibalaji.ap.gov.in/sdn/rest/v1/acc/get_availability?for=dashboard&location=TIRUMALA"
-    payload={}
-    headers = {
-      'authority': 'online.tirupatibalaji.ap.gov.in',
-      'accept': 'application/json',
-      'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-      'authtoken': '16202e94375972563f5f563e2647eb58b690c22d-1245772922',
-      'content-type': 'application/json',
-      'cookie': '_ga=GA1.5.1068875658.1682002300; _gid=GA1.5.1199907862.1689691573; _ga=GA1.1.1068875658.1682002300; _gat_UA-56816637-81=1; user_id=9441154; authtoken=16202e94375972563f5f563e2647eb58b690c22d-1245772922; valid_till=2024-07-21 10:24:32; valid_till_in_utc=2024-07-21 04:54:32; mobileNum=8669175196; _ga_MT8JLQ6M96=GS1.1.1689913461.25.1.1689913479.0.0.0',
-      'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
-      'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
-      'sec-ch-ua-mobile': '?1',
-      'sec-ch-ua-platform': '"Android"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-origin',
-      'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36',
-      'userid': '9441154'
-    }
-    logger.warning("url: "+str(url)+" | payload: "+str(payload)+ "| headers: " +str(headers)) 
-    try:
+    while True:
+      url = "https://online.tirupatibalaji.ap.gov.in/sdn/rest/v1/acc/get_availability?for=dashboard&location=TIRUMALA"
+      payload={}
+      headers = {
+          'authority': 'online.tirupatibalaji.ap.gov.in',
+          'accept': 'application/json',
+          'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+          'authtoken': '16202e94375972563f5f563e2647eb58b690c22d-1245772922',
+          'content-type': 'application/json',
+          'cookie': '_ga=GA1.5.1068875658.1682002300; _gid=GA1.5.1199907862.1689691573; _ga=GA1.1.1068875658.1682002300; _gat_UA-56816637-81=1; user_id=9441154; authtoken=16202e94375972563f5f563e2647eb58b690c22d-1245772922; valid_till=2024-07-21 10:24:32; valid_till_in_utc=2024-07-21 04:54:32; mobileNum=8669175196; _ga_MT8JLQ6M96=GS1.1.1689913461.25.1.1689913479.0.0.0',
+          'referer': 'https://online.tirupatibalaji.ap.gov.in/accommodation/slot-booking?flow=acc&flowIdentifier=acc',
+          'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+          'sec-ch-ua-mobile': '?1',
+          'sec-fetch-dest': 'empty',
+          'sec-fetch-mode': 'cors',
+          'sec-fetch-site': 'same-origin',
+          'userid': '9441154'
+        }
+      proceed = true
+      try:
         response = requests.request("GET", url, headers=headers, data=payload)
-        logger.warning("response received: "+str(response))
-    except:
-        logger.critical("ERROR from TTD")
-    if(not response.json().get('status') == 'fail'):
+      except Exception:
+        proceed = false
+        logger.critical("ERROR from TTD") 
+        logger.warning("response: "+str(response) + "Canot Proceed") 
+        logger.warning(Exception) 
+      if(proceed and not response.json().get('status') == 'fail'):
         avl1 = (response.json().get('result').get('20230723').get('avl'))
         avl2 = 0
         if (avl1==0): 
